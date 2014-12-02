b85c1754aca7f61cb75b538226de368fb46c3c88
Denis Plotkin <Denis.Plotkin@gmail.com>
2014-12-02 15:38:40 -0500

    added city

---
+++ City.cpp
@@ -0,0 +1,20 @@
+#include "City.h"
+using namespace std;
+
+City::City(){
+  //things
+}
+
+
+City::City(string city){
+  name = city;
+}
+
+void City::addFlight(Flight f){
+  flights.push_back(f);
+}
+string City::getName(){
+  return name;
+}
+
+
---
+++ City.h
@@ -0,0 +1,28 @@
+#ifndef City_H_
+#define City_H_
+
+class City;
+
+#include "Flight.h"
+#include "Time.h"
+#include "Date.h"
+#include <string>
+#include <vector>
+using namespace std;
+
+class City{
+ public:
+  City();
+  City(string name);
+  void addFlight(Flight f);
+  string getName();
+  //add how to get flights in the future
+ private:
+  string name;
+  vector flights;
+
+};
+
+#endif
+  
+  
